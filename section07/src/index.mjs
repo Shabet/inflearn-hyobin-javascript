c8697328de863ede10bb9eaff3dc64a4141fa96b
Shabet <pakjunyoung@nate.com>
2025-01-08 16:15:07 +0900

    ~section07 내용 추가

---
+++ section07/src/index.mjs
@@ -0,0 +1,6 @@
+// import {num, add, User} from './test.mjs';
+import * as testModule from './test.mjs';
+
+console.log(testModule.num);
+console.log(testModule.add(10, 19));
+console.log(new testModule.User('효빈'));
